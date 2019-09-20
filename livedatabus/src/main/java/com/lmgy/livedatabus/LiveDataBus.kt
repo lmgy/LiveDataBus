f7c96ac0e063c32ae8503c061db126f45becf6ef
lmgy <luomusue@gmail.com>
2019-09-20 17:04:05 +0800

    refractor: add singleInstance

--- livedatabus/src/main/java/com/lmgy/livedatabus/LiveDataBus.kt
+++ livedatabus/src/main/java/com/lmgy/livedatabus/LiveDataBus.kt
@@ -6,7 +6,7 @@ import java.util.concurrent.ConcurrentHashMap
  * @author lmgy
  * @date 2019/9/17
  */
-object LiveDataBus {
+class LiveDataBus private constructor(){
 
     private val subjectMap = ConcurrentHashMap<Class<*>, LiveDataEvent<*>>()
 
@@ -15,5 +15,15 @@ object LiveDataBus {
     }
 
     fun <T> with(clazz: Class<T>) = bus(clazz) as LiveDataEvent<T>
+
+    companion object {
+        @Volatile
+        private var instance: LiveDataBus? = null
+
+        @JvmStatic
+        fun getInstance() = instance ?: synchronized(this) {
+            instance ?: LiveDataBus().also { instance = it }
+        }
+    }
 
 }
